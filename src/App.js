a56f2ca6b431313277804a0b3e7ff9f2735bdff0
mmitch2958 <mikescottmitchell@outlook.com>
2020-09-17 12:47:34 -0400

    update header

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+  // const [date, setDate] = useState(null);
+  // useEffect(() => {
+  //   async function getDate() {
+  //     const res = await fetch('/api/date');
+  //     const newDate = await res.text();
+  //     setDate(newDate);
+  //   }
+  //   getDate();
+  // }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Mike Mitchell Create React App + Go API</h1>
       <h2>
         Deployed with{' '}
         <a
@@ -48,8 +48,8 @@ function App() {
         .
       </p>
       <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      {/* <h2>The date according to Go is:</h2>
+      <p>{date ? date : 'Loading date...'}</p> */}
     </main>
   );
 }
